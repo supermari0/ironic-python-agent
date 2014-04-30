3e1471c8de90550ab2fdc435baeb10759c1a6777
Jim Rollenhagen <jim@jimrollenhagen.com>
2014-04-29 17:18:38 -0700

    Use processutils properly
    Previously this code was calling processutils.execute incorrectly,
    expecting it to return the exit code. This change:
    
    * fixes calls to processutils.execute
    * fixes related tests
    * changes exceptions triggered by shell command failure to
      subclass processutils.ProcessExecutionError, to properly capture
      commands, stdout, stderr, etc. from processutils.execute().
    
    Change-Id: I3343f104a5a8200d53bd87a8799ff918e375b17f
    (cherry picked from commit 0e4acce6cb9ec9edd62b3a19a60a951fc0509849)

--- ironic_python_agent/extensions/standby.py
+++ ironic_python_agent/extensions/standby.py
@@ -25,6 +25,7 @@ from ironic_python_agent import errors
 from ironic_python_agent.extensions import base
 from ironic_python_agent import hardware
 from ironic_python_agent.openstack.common import log
+from ironic_python_agent.openstack.common import processutils
 from ironic_python_agent import utils
 
 LOG = log.getLogger(__name__)
@@ -50,9 +51,14 @@ def _write_image(image_info, device):
     script = _path_to_script('shell/write_image.sh')
     command = ['/bin/bash', script, image, device]
     LOG.info('Writing image with command: {0}'.format(' '.join(command)))
-    exit_code = utils.execute(*command)
-    if exit_code != 0:
-        raise errors.ImageWriteError(exit_code, device)
+    try:
+        stdout, stderr = utils.execute(*command, check_exit_code=[0])
+    except processutils.ProcessExecutionError as e:
+        raise errors.ImageWriteError(stdout=e.stdout,
+                                     stderr=e.stderr,
+                                     exit_code=e.exit_code,
+                                     cmd=e.cmd,
+                                     description=e.description)
     totaltime = time.time() - starttime
     LOG.info('Image {0} written to device {1} in {2} seconds'.format(
              image, device, totaltime))
@@ -82,10 +88,15 @@ def _write_configdrive_to_partition(configdrive, device):
     command = ['/bin/bash', script, filename, device]
     LOG.info('copying configdrive to disk with command {0}'.format(
              ' '.join(command)))
-    exit_code = utils.execute(*command)
 
-    if exit_code != 0:
-        raise errors.ConfigDriveWriteError(exit_code, device)
+    try:
+        stdout, stderr = utils.execute(*command, check_exit_code=[0])
+    except processutils.ProcessExecutionError as e:
+        raise errors.ConfigDriveWriteError(stdout=e.stdout,
+                                           stderr=e.stderr,
+                                           exit_code=e.exit_code,
+                                           cmd=e.cmd,
+                                           description=e.description)
 
     totaltime = time.time() - starttime
     LOG.info('configdrive copied from {0} to {1} in {2} seconds'.format(
@@ -203,6 +214,11 @@ class StandbyExtension(base.BaseAgentExtension):
         LOG.info('Rebooting system')
         command = ['/bin/bash', script]
         # this should never return if successful
-        exit_code = utils.execute(*command)
-        if exit_code != 0:
-            raise errors.SystemRebootError(exit_code)
+        try:
+            stdout, stderr = utils.execute(*command, check_exit_code=[0])
+        except processutils.ProcessExecutionError as e:
+            raise errors.SystemRebootError(stdout=e.stdout,
+                                           stderr=e.stderr,
+                                           exit_code=e.exit_code,
+                                           cmd=e.cmd,
+                                           description=e.description)
