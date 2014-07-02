87c0c3b3f5d38270dbb379b6227da96019723e61
Josh Gachnang <josh@pcsforeducation.com>
2014-07-02 16:48:54 -0700

    Adding support for decommissioning
    The pluggable hardware manager should be able be used for decommissioning,
    such as erasing disks, updating firmwares, changing BIOS settings, and
    anything else specific hardware needs. This works with the agent driver
    to store state in Ironic and request out of band reboots (which may be
    required to make BIOS/firmware updates work). Also allows the agent to do
    some basic verification of the node's properties, to ensure they haven't
    changed, such as a stick of memory dying.
    
    Change-Id: Ia42a8e75d2b8887b2351de97c2137fe21a7f4041
    (cherry picked from commit 68804bd6f0404bca65452129372ade1358cd832d)

--- ironic_python_agent/extensions/decom.py
+++ ironic_python_agent/extensions/decom.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from ironic_python_agent import errors
 from ironic_python_agent.extensions import base
 from ironic_python_agent import hardware
 
@@ -19,4 +20,28 @@ from ironic_python_agent import hardware
 class DecomExtension(base.BaseAgentExtension):
     @base.async_command('erase_hardware')
     def erase_hardware(self):
-        hardware.get_manager().erase_devices()
+        return hardware.get_manager().erase_devices()
+
+    @base.async_command('get_decommission_steps')
+    def get_decommission_steps(self):
+        # Results should be a dict, not a list
+        manager = hardware.get_manager()
+        return {
+            'decommission_steps': manager.get_decommission_steps(),
+        }
+
+    @base.async_command('decommission')
+    def decommission(self, node=None, ports=None, driver_info=None, **kwargs):
+        if not ports:
+            ports = []
+
+        # Current way
+        if node and not driver_info:
+            return hardware.get_manager().decommission(node, ports, **kwargs)
+        # Backwards compatibility
+        elif driver_info and not node:
+            node = {'driver_info': driver_info}
+            return hardware.get_manager().decommission(node, [], **kwargs)
+        else:
+            raise errors.InvalidContentError(
+                'Either provide "node" and "ports" params, or "driver_info".')
